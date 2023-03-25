2af4d4c394fca8c2d91dd2e86644a90c2656cd8c
Luis <luisrc91@live.com>
2023-03-25 10:15:16 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+
 
 function App() {
   let [searchTerm, setSearchTerm] = useState('')
@@ -33,7 +35,9 @@ function App() {
     <div className="App">
       <SearchBar handleSearch={handleSearch} />
       {message}
+      <DataContext.Provider value={{ data}}>
       <Gallery data={data} />
+      </DataContext.Provider>
     </div>
   );
 }
